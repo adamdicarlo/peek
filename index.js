d1bb340767d536a60d782f4ee80923f91e5b0096
Adam J. DiCarlo <adam.dicarlo@gmail.com>
2013-12-01 20:48:27 -0800

    Replace the loop with recursion for fun.

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-module.exports = function(path) {
+module.exports = function peek(path) {
   if (typeof path !== 'string') {
     throw new TypeError('path must be a string')
   }
-  var parts = path.split('.')
+
   return function(obj) {
-    return parts.reduce(function(obj, segment) {
-      if(obj === null || typeof obj !== 'object') {
-        return undefined
-      }
-      return obj[segment]
-    }, obj)
+    return _peek(obj, path.split('.'))
+  }
+
+  function _peek(obj, parts) {
+    if(!parts.length) {
+      return obj
+    }
+
+    if(obj === null || typeof obj !== 'object') {
+      return undefined
+    }
+    return _peek(obj[parts[0]], parts.slice(1))
   }
 }
