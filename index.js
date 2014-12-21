2001e1365d38f776f77f15c077e3f98c38b86170
Adam J. DiCarlo <adam.dicarlo@gmail.com>
2014-12-20 19:12:46 -0800

    Name anonymous functions.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-module.exports = function(path) {
+module.exports = function createPeek(path) {
   if (typeof path !== 'string') {
     throw new TypeError('path must be a string')
   }
   var parts = path.split('.')
-  return function(obj) {
-    return parts.reduce(function(obj, segment) {
+  return function peek(obj) {
+    return parts.reduce(function peekSegment(obj, segment) {
       if (obj === null || obj === undefined) {
         return undefined
       }
